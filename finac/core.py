a5f233807434e370c6312d4e3096dd759378d6f5
Sergei S <div@altertech.com>
2019-10-30 13:05:42 +0100

    0.1.5

--- finac/core.py
+++ finac/core.py
@@ -2,7 +2,7 @@ __author__ = 'Altertech, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2019 Altertech'
 __license__ = 'MIT'
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from sqlalchemy.exc import IntegrityError
 
