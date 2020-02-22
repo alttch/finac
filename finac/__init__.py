a0c8e67d23fc51e8d50183377f2104b7dee7ff9a
Sergei S <div@altertech.com>
2020-02-22 23:34:44 +0100

    0.2.25

--- finac/__init__.py
+++ finac/__init__.py
@@ -2,7 +2,7 @@ __author__ = 'Altertech, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2019 Altertech'
 __license__ = 'MIT'
 
-__version__ = '0.2.24'
+__version__ = '0.2.25'
 
 import rapidtables
 import neotermcolor
