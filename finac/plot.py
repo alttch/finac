dea3c7bda37dc211145007b6a0934c97c57ae513
Sergei S <div@altertech.com>
2020-02-28 01:19:21 +0100

    0.3.0

--- finac/plot.py
+++ finac/plot.py
@@ -2,7 +2,7 @@ __author__ = 'Altertech, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2019 Altertech'
 __license__ = 'MIT'
 
-__version__ = '0.2.28'
+__version__ = '0.3.0'
 
 from . import core
 
