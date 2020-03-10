d9c1953ca24077485c131db2841e8dacff364a3a
Sergei S <div@altertech.com>
2020-03-10 23:05:43 +0100

    multi processor benchmark

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -15,6 +15,7 @@ import random
 import time
 
 from types import SimpleNamespace
+from concurrent.futures import ProcessPoolExecutor
 
 TEST_DB = '/tmp/finac-test.db'
 
@@ -28,12 +29,12 @@ if __name__ == '__main__':
                     '--account-number',
                     help='accounts to create',
                     type=int,
-                    default=10)
+                    default=100)
     ap.add_argument('-n',
                     '--transaction-number',
                     help='transactions per account',
                     type=int,
-                    default=10)
+                    default=100)
     ap.add_argument('--no-keeper',
                     help='disable built-in integrity keeper',
                     action='store_true')
@@ -44,7 +45,9 @@ if __name__ == '__main__':
                     help='DB connection string (WARNING! ALL DATA WILL BE LOST',
                     metavar='DBCONN',
                     default=TEST_DB)
+    ap.add_argument('-w', '--workers', help='Max finac workers', type=int)
     a = ap.parse_args()
+    pool = ProcessPoolExecutor(max_workers=a.workers)
     if not a.benchmark_only:
         if a.dbconn == TEST_DB:
             try:
@@ -53,6 +56,13 @@ if __name__ == '__main__':
                 pass
     finac.init(db=a.dbconn, keep_integrity=not a.no_keeper, multiplier=100)
     finac.core.rate_cache = None
+    futures = []
+
+    def wait_futures():
+        for f in futures:
+            f.result()
+        futures.clear()
+
     if not a.benchmark_only:
         print('Cleaning up...')
         # cleanup
@@ -63,7 +73,9 @@ if __name__ == '__main__':
         print('Creating accounts...')
         # create accounts
         for x in tqdm(range(1, a.account_number + 1), leave=True):
-            finac.account_create(f'account-{x}', 'USD')
+            futures.append(
+                pool.submit(finac.account_create, f'account-{x}', 'USD'))
+        wait_futures()
         # generate transactions
         print('Generating transactions...')
         for i in tqdm(range(a.transaction_number), leave=True):
@@ -71,25 +83,34 @@ if __name__ == '__main__':
                 dt_id = x
                 while dt_id == x:
                     dt_id = random.randint(1, a.account_number)
-                finac.mv(dt=f'account-{dt_id}',
-                         ct=f'account-{x}',
-                         amount=random.randint(1000, 10000) / 1000.0,
-                         tag=f'trans {x}')
+                futures.append(
+                    pool.submit(finac.mv,
+                                dt=f'account-{dt_id}',
+                                ct=f'account-{x}',
+                                amount=random.randint(1000, 10000) / 1000.0,
+                                tag=f'trans {x}'))
+            wait_futures()
     print('Testing...')
     t = time.time()
     for x in tqdm(range(1, a.account_number + 1), leave=True):
         dt_id = x
         while dt_id == x:
             dt_id = random.randint(1, a.account_number)
-        finac.mv(dt=f'account-{dt_id}',
-                 ct=f'account-{x}',
-                 amount=random.randint(1000, 10000) / 1000.0,
-                 tag=f'trans {x}')
+        futures.append(
+            pool.submit(finac.mv,
+                        dt=f'account-{dt_id}',
+                        ct=f'account-{x}',
+                        amount=random.randint(1000, 10000) / 1000.0,
+                        tag=f'trans {x}'))
+    wait_futures()
     print('Average transaction time: {:.3f}ms'.format(
         (time.time() - t) / a.account_number * 1000))
     t = time.time()
     for x in tqdm(range(1, a.account_number + 1), leave=True):
-        finac.account_statement_summary(f'account-{x}', start='2019-01-01')
+        futures.append(pool.submit(finac.account_statement_summary,
+                       f'account-{x}',
+                       start='2019-01-01'))
+    wait_futures()
     print('Average statement time: {:.3f}ms'.format(
         (time.time() - t) / a.account_number * 1000))
     if not a.benchmark_only:
