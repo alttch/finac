748aedb5a17eb815d92791a98dd9f49de63c901f
Sergei S <div@altertech.com>
2020-05-04 04:05:50 +0200

    0.4.34

--- finac/core.py
+++ finac/core.py
@@ -2,7 +2,7 @@ __author__ = 'Altertech, https://www.altertech.com/'
 __copyright__ = 'Copyright (C) 2019 Altertech'
 __license__ = 'MIT'
 
-__version__ = '0.4.33'
+__version__ = '0.4.34'
 
 from sqlalchemy.exc import IntegrityError
 from cachetools import TTLCache
